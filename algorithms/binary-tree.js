44aca3bf7fa744725ea1a84aa1297b0679ba381a
Nischal Kumar <nischal.vainshir@gmail.com>
2023-10-07 20:07:24 +0530

    Added a new algorithm, a binary tree Need to update test for Binary Tree.

---
+++ algorithms/binary-tree.js
@@ -0,0 +1,96 @@
+
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+
+    constructor() {
+        this.root = null;
+    }
+
+    add(element) {
+
+        let newNode = new TreeNode(element);
+        if(this.isTreeNull(this.root)) {
+            this.root = newNode;
+            return;
+        }
+
+        let current = this.root;
+        while (current) {
+
+            if(element === current.data) return;
+
+            if(element < current.data) {
+                if(this.isTreeNull(current.left)) {
+                    current.left = new TreeNode(element);
+                    return;
+                }
+                current = current.left;
+            }
+
+            if (element > current.data) {
+                if(this.isTreeNull(current.right)) {
+                    current.right = new TreeNode(element);
+                    return;
+                }
+                current = current.right;
+            }
+        }
+
+    }
+
+    isTreeNull(node) {
+        return node === null;
+    }
+
+    search(element) {
+
+        if(!this.root) return false;
+
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if(element < current.data){
+                current = current.left;
+            } else if(element > current.data) {
+                current = current.right;
+            } else {
+                found = current;
+            }
+        }
+
+        if (found) {
+            console.log('Found element');
+        } else {
+            console.log('Not found element');
+        }
+
+    }
+
+    display() {
+        console.log('root', JSON.stringify(this.root));
+    }
+
+}
+
+
+const bt = new BinaryTree();
+bt.add(9);
+bt.add(2);
+bt.add(8);
+bt.add(6);
+bt.add(16);
+bt.add(1);
+bt.add(19);
+bt.add(11);
+bt.search(212);
+bt.display();
+
+
+module.exports = BinaryTree
